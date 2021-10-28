80715b68ff476e0997f8a3c947f59030ac27a0ce
ABDALLAH EL MOUAATAMID <abdaelmouaatamid@gmail.com>
2021-10-28 22:07:50 +0200

    final

--- Lab03/program_2_c.s
+++ Lab03/program_2_c.s
@@ -51,25 +51,7 @@ s.d f8,v6(r2)
 div.d f9,f8,f1
 s.d f9,v7(r2)
 ;;;;;;;;;;;;;;;;;;;;;;
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;
-l.d f7,v1(r2); carico v1
-l.d f1,v2(r2);carico v2
-l.d f2,v3(r2);carico v3
-daddi r1,r1,-1
-mul.d f3,f1,f2;calcolo prodotto v2*v3
-l.d f4,v4(r2)
-add.d f7,f7,f3;sommo v1 e v2*v3
-s.d f7,v5(r2);contiene f5
-;;;;;;;;;;;;;;;;;;;;;;;;
-mul.d f8,f7,f4
-s.d f8,v6(r2)
-;;;;;;;;;;;;;;;;;;;;;;;
-div.d f9,f8,f1
-s.d f9,v7(r2)
-;;;;;;;;;;;;;;;;;;;;;;
-
+daddi r2,r2,8
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
 l.d f7,v1(r2); carico v1
@@ -87,6 +69,7 @@ s.d f8,v6(r2)
 div.d f9,f8,f1
 s.d f9,v7(r2)
 ;;;;;;;;;;;;;;;;;;;;;;
+daddi r2,r2,8
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
 l.d f7,v1(r2); carico v1
@@ -104,7 +87,25 @@ s.d f8,v6(r2)
 div.d f9,f8,f1
 s.d f9,v7(r2)
 ;;;;;;;;;;;;;;;;;;;;;;
-daddi r2,r2,32
+daddi r2,r2,8
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+l.d f7,v1(r2); carico v1
+l.d f1,v2(r2);carico v2
+l.d f2,v3(r2);carico v3
+daddi r1,r1,-1
+mul.d f3,f1,f2;calcolo prodotto v2*v3
+l.d f4,v4(r2)
+add.d f7,f7,f3;sommo v1 e v2*v3
+s.d f7,v5(r2);contiene f5
+;;;;;;;;;;;;;;;;;;;;;;;;
+mul.d f8,f7,f4
+s.d f8,v6(r2)
+;;;;;;;;;;;;;;;;;;;;;;;
+div.d f9,f8,f1
+s.d f9,v7(r2)
+;;;;;;;;;;;;;;;;;;;;;;
+daddi r2,r2,8
 
 bnez r1, cycle
 halt
