1efa927236179ab0a3a6032d1f35b602a537bf9b
ABDALLAH EL MOUAATAMID <abdaelmouaatamid@gmail.com>
2021-10-28 23:04:20 +0200

    YO

--- Lab03/program_2_a.s
+++ Lab03/program_2_a.s
@@ -36,11 +36,11 @@ lb r1,DIM(r0)
 daddi r2,r0,0
 cycle:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
-l.d f7,v1(r2); carico v1
 l.d f1,v2(r2);carico v2
 l.d f2,v3(r2);carico v3
 daddi r1,r1,-1
 mul.d f3,f1,f2;calcolo prodotto v2*v3
+l.d f7,v1(r2); carico v1
 l.d f4,v4(r2)
 add.d f7,f7,f3;sommo v1 e v2*v3
 s.d f7,v5(r2);contiene f5
