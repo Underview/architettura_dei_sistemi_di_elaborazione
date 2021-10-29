d6c10dd08f2209a680cd0bf694b9280d33acb37d
ABDALLAH EL MOUAATAMID <abdaelmouaatamid@gmail.com>
2021-10-29 13:51:15 +0200

    yes

--- Lab03/program_2_c.s
+++ Lab03/program_2_c.s
@@ -34,32 +34,25 @@ DIM: .byte 40
 main:
 lb r1,DIM(r0)
 daddi r2,r0,0
+daddi r3,r0,0
+daddi r4,r0,0
+daddi r5,r0,0
 cycle:
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
-l.d f7,v1(r2); carico v1
-l.d f1,v2(r2);carico v2
-l.d f2,v3(r2);carico v3
-daddi r1,r1,-1
-mul.d f3,f1,f2;calcolo prodotto v2*v3
-l.d f7,v1(r2); carico v1
-l.d f4,v4(r2)
-add.d f7,f7,f3;sommo v1 e v2*v3
-s.d f7,v5(r2);contiene f5
-;;;;;;;;;;;;;;;;;;;;;;;;
-mul.d f8,f7,f4
-s.d f8,v6(r2)
-;;;;;;;;;;;;;;;;;;;;;;;
-div.d f9,f8,f1
-s.d f9,v7(r2)
-;;;;;;;;;;;;;;;;;;;;;;
+daddi r3,r3,8 ; cambia nome
 daddi r2,r2,8 ; 
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;
 l.d f7,v1(r2); carico v1
 l.d f1,v2(r2);carico v2
 l.d f2,v3(r2);carico v3
 daddi r1,r1,-1
+daddi r1,r1,-1
+mul.d f11,f5,f10;calcolo prodotto v2*v3
 mul.d f3,f1,f2;calcolo prodotto v2*v3
+l.d f6,v1(r3); carico v1
+l.d f5,v2(r3);carico v2
+l.d f10,v3(r3);carico v3
 l.d f7,v1(r2); carico v1
 l.d f4,v4(r2)
 add.d f7,f7,f3;sommo v1 e v2*v3
@@ -71,26 +64,43 @@ s.d f8,v6(r2)
 div.d f9,f8,f1
 s.d f9,v7(r2)
 ;;;;;;;;;;;;;;;;;;;;;;
-daddi r2,r2,8 ; cambia nome
+
+;f1,f2,f3,f4,f7,f8,f9;
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+daddi r1,r1,-1
+mul.d f11,f5,f10;calcolo prodotto v2*v3
+l.d f6,v1(r3); carico v1
+l.d f12,v4(r3)
+add.d f6,f6,f11;sommo v1 e v2*v3
+s.d f6,v5(r3);contiene f5
+;;;;;;;;;;;;;;;;;;;;;;;;
+mul.d f14,f6,f12
+s.d f14,v6(r3)
+;;;;;;;;;;;;;;;;;;;;;;;
+div.d f13,f14,f5
+s.d f13,v7(r3)
+;;;;;;;;;;;;;;;;;;;;;;
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
-l.d f7,v1(r2); carico v1
-l.d f1,v2(r2);carico v2
-l.d f2,v3(r2);carico v3
+l.d f7,v1(r3); carico v1
+l.d f1,v2(r3);carico v2
+l.d f2,v3(r3);carico v3
 daddi r1,r1,-1
 mul.d f3,f1,f2;calcolo prodotto v2*v3
-l.d f7,v1(r2); carico v1
-l.d f4,v4(r2)
+l.d f7,v1(r3); carico v1
+l.d f4,v4(r3)
 add.d f7,f7,f3;sommo v1 e v2*v3
-s.d f7,v5(r2);contiene f5
+s.d f7,v5(r3);contiene f5
 ;;;;;;;;;;;;;;;;;;;;;;;;
 mul.d f8,f7,f4
-s.d f8,v6(r2)
+s.d f8,v6(r3)
 ;;;;;;;;;;;;;;;;;;;;;;;
 div.d f9,f8,f1
-s.d f9,v7(r2)
+s.d f9,v7(r3)
 ;;;;;;;;;;;;;;;;;;;;;;
-daddi r2,r2,8 ; cambia nome
+daddi r4,r4,8 ; cambia nome
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -112,5 +122,5 @@ s.d f9,v7(r2)
 
 
 bnez r1, cycle
-daddi r2,r2,8;cambia
+daddi r5,r5,8;cambia
 halt
